283e4cfcfebebfdb2ddfc37417d7a5ba95b89861
Michele Adduci <adduci.michele@gmail.com>
2014-03-04 16:01:15 +0100

    Fixed some CMakeLists.txt minor problems

--- cmake/Modules/FindDC1394.cmake
+++ cmake/Modules/FindDC1394.cmake
@@ -1,9 +1,6 @@
 #############################################################################
 #
-# $Id: FindDC1394.cmake 4056 2013-01-05 13:04:42Z fspindle $
-#
-# This file is part of the ViSP software.
-# Copyright (C) 2005 - 2013 by INRIA. All rights reserved.
+# $Id: FindDC1394.cmake 2014-03-04 13:04:42Z madduci $
 # 
 # This software is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -11,22 +8,6 @@
 # See the file LICENSE.txt at the root directory of this source
 # distribution for additional information about the GNU GPL.
 #
-# For using ViSP with software that can not be combined with the GNU
-# GPL, please contact INRIA about acquiring a ViSP Professional 
-# Edition License.
-#
-# See http://www.irisa.fr/lagadic/visp/visp.html for more information.
-# 
-# This software was developed at:
-# INRIA Rennes - Bretagne Atlantique
-# Campus Universitaire de Beaulieu
-# 35042 Rennes Cedex
-# France
-# http://www.irisa.fr/lagadic
-#
-# If you have questions regarding the use of this file, please contact
-# INRIA at visp@inria.fr
-# 
 # This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 # WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 #
@@ -47,17 +28,7 @@
 # DC1394_2_LIBRARY
 #
 # The two defines below are only useful to compile with libdc1394-2.x. In
-# that case DC1394_VERSION=2. Since the libdc1394-2.x API is not stable, we 
-# need to determine if dc1394_find_cameras() or dc1394_enumerate_cameras() 
-# functions are available. dc1394_enumerate_cameras() was introduced after 
-# libdc1394-2.0.0-rc7. DC1394_CAMERA_ENUMERATE_FOUND is TRUE when 
-# dc1394_camera_enumerate() function is found. DC1394_FIND_CAMERAS_FOUND is 
-# TRUE when dc1394_find_cameras() is found.
-# DC1394_CAMERA_ENUMERATE_FOUND
-# DC1394_FIND_CAMERAS_FOUND
-#
-# Authors:
-# Fabien Spindler
+# that case DC1394_VERSION=2. 
 #
 #############################################################################
 
@@ -112,53 +83,11 @@ ELSE(NOT UNIX)
 
   IF(DC1394_2_LIBRARY AND DC1394_2_INCLUDE_DIR)
 
-    # Since the libdc1394-2.x API is not stable, try to compile a
-    # sample code to determine if we have to use dc1394_find_cameras() or
-    # dc1394_enumerate_cameras() functions. dc1394_enumerate_cameras() was 
-    # introduced after libdc1394-2.0.0-rc7
-
-    include(CheckCXXSourceCompiles)
-	
     SET(CMAKE_REQUIRED_LIBRARIES ${DC1394_2_LIBRARY})
     SET(CMAKE_REQUIRED_INCLUDES ${DC1394_2_INCLUDE_DIR})
-	
-    CHECK_CXX_SOURCE_COMPILES("
-      #include <dc1394/control.h>
-      #include <dc1394/utils.h>
-
-      int main(){
-        dc1394_t * d;
-        dc1394camera_list_t * list;
-        d = dc1394_new ();
-        dc1394_camera_enumerate (d, &list);
-        return 0;
-      }
-      " DC1394_CAMERA_ENUMERATE_FOUND) 
-    #MESSAGE("DC1394_CAMERA_ENUMERATE_FOUND: ${DC1394_CAMERA_ENUMERATE_FOUND}")
-
-    IF(NOT DC1394_CAMERA_ENUMERATE_FOUND)
-      # Try to test the compatibility to libdc1394-2.0.0-rc7
-      CHECK_CXX_SOURCE_COMPILES("
-        #include <dc1394/control.h>
-        #include <dc1394/utils.h>
-
-        int main(){
-          dc1394camera_t **cameras;
-          unsigned int num_cameras;
-          dc1394_find_cameras(&cameras, &num_cameras);
-          return 0;
-        }
-        " DC1394_FIND_CAMERAS_FOUND) 
-      #MESSAGE("DC1394_FIND_CAMERAS_FOUND: ${DC1394_FIND_CAMERAS_FOUND}")
-    ENDIF(NOT DC1394_CAMERA_ENUMERATE_FOUND)
-
-    IF(NOT DC1394_CAMERA_ENUMERATE_FOUND AND NOT DC1394_FIND_CAMERAS_FOUND)
-       SET(DC1394_2_FOUND FALSE)
-       MESSAGE("libdc1394-2.x found but not compatible with ViSP...")
-    ELSE(NOT DC1394_CAMERA_ENUMERATE_FOUND AND NOT DC1394_FIND_CAMERAS_FOUND)
-       SET(DC1394_FOUND TRUE)
-       SET(DC1394_2_FOUND TRUE)
-    ENDIF(NOT DC1394_CAMERA_ENUMERATE_FOUND AND NOT DC1394_FIND_CAMERAS_FOUND)
+	    
+    SET(DC1394_FOUND TRUE)
+    SET(DC1394_2_FOUND TRUE)
 
     SET(DC1394_VERSION 2)
     SET(DC1394_LIBRARIES ${DC1394_2_LIBRARY})
@@ -166,8 +95,6 @@ ELSE(NOT UNIX)
     SET(DC1394_INCLUDE_DIRS ${DC1394_2_INCLUDE_DIRS})
   ENDIF(DC1394_2_LIBRARY AND DC1394_2_INCLUDE_DIR)
   
-
-
   ## --------------------------------
 
   MARK_AS_ADVANCED(
